31d40753ce59856d7efa33a4c868ed3d596eb05e
antoniogiachin <giachinantonio@outlook.com>
2022-02-01 21:51:22 +0100

    nuovo approccio

---
+++ debug/debug2.js
@@ -0,0 +1,85 @@
+// Mi riferisco a bottone play
+const play = document.getElementById('play');
+
+/* ----- FUNZIONI ------ */
+// Funzione per numero casuale
+function casualNumber (min, max){
+    return Math.floor(Math.random() * (max - min+1)+min);
+}
+
+// funzione per creazione bombe
+
+function createBombArray(bombNumber, maxValue){
+
+    const bombArray=[];
+
+    while(bombArray.length < bombNumber){
+
+        const bombId = casualNumber(1, maxValue);
+
+        // Evito che due numeri si ripetano
+        if(!bombArray.includes(bombId)){
+            bombArray.push(bombId);
+            
+        }
+
+    }
+
+    console.log(bombArray);
+}
+
+
+
+// Funzione che crea grid
+function createGrid(value){
+
+    // Mi riferisco a div #grid
+    const gridHtml = document.getElementById('grid');
+
+    // Svuoto grid
+    gridHtml.innerHTML ='';
+
+    // Ciclo for e creo squares a seconda del value inserito
+    for(let i = 1; i <= value; i++){
+
+        const square = document.createElement('div');
+
+        square.classList.add('square');
+
+        square.innerText = i;
+
+        // square.addEventListener aggiungi dopo direttamente funzione
+
+        // dimensione a seconda di value
+        if(value == 100){
+            square.classList.add('sq-easy');
+        } else if (value == 81){
+            square.classList.add('sq-medium');
+        } else {
+            square.classList.add('sq-hard');
+        }
+
+        gridHtml.appendChild(square);
+    }
+
+
+}
+
+
+
+// Ascoltatore di eventi per play
+play.addEventListener('click',
+
+    function(){
+        // Richiamo il valore
+        const difficultyValue = document.getElementById('difficulty-select').value;
+
+        createGrid(difficultyValue);
+
+        createBombArray(16, difficultyValue);
+
+
+    }
+
+);
+
